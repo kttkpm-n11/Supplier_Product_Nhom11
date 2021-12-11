618a38051da0bae36c1774bce82255d9b002a4fb
datma <datma113112111@gmail.com>
2021-12-11 14:47:32 +0700

    add register and login ui

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import Dashbroad from "./pages/Dashbroad";
 import LoginPage from "./pages/LoginPage";
+import Register from "./pages/Register";
 const routes = [
     { path: ["/"], component: LoginPage, exact: true },
-    { path: ["/dashbroad"], component: Dashbroad, exact: true },
+    { path: ["/dashboard"], component: Dashbroad, exact: true },
+    { path: ["/register"], component: Register, exact: true },
    
         
    
